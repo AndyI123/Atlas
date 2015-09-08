9f3036029cda685cb1e8e89ffe1ddd625d3403b6
Andrew Ilyas <ailyas@mit.edu>
2015-09-08 13:20:26 -0400

    Starting homescreen

--- GridNotes.js
+++ GridNotes.js
@@ -1,8 +1,13 @@
 var itemIndex = 4;
-
 function randomId() {
     return '_' + Math.random().toString(36).substr(2, 9);
 }
+
+Router.route("/flowchart");
+Router.route("/", function() {
+    this.render("home")
+})
+
 
 if (Meteor.isClient) {
     
@@ -14,11 +19,17 @@ if (Meteor.isClient) {
       $("#defaultradio").attr('checked', true);
   })
     
+  Template.home.startup(function () {
+      jsPlumb.ready(function() {
+          jsPlumb.setContainer($("#bg"));
+      });
+      
+  })
+  
   Template.notetaker.events({
       'submit .new-bullet' : function (e) {
           e.preventDefault();
           var newState = $('<div>').attr('id', randomId()).addClass('item');
-
           var title = $('<div>').addClass('title').text($("#newnote").val());
           var connect = $('<div>').addClass('connect');
 
@@ -48,6 +59,5 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
   });
 }
