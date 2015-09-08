a5af95678e3926916f14b16e42795c9fc28f548e
Andrew Ilyas <ailyas@mit.edu>
2015-09-08 19:37:23 -0400

    Moving graph

--- GridNotes.js
+++ GridNotes.js
@@ -1,6 +1,124 @@
-var itemIndex = 4;
+/**Utitlity functions**/
+var paper, circs, i, nowX, nowY, timer, props = {}, toggler = 0, elie, dx, dy, rad, cur, opa, c, lines, lineIds, circIds;
+
+circIds = [];
+lineIds = []
+
+
 function randomId() {
     return '_' + Math.random().toString(36).substr(2, 9);
+}
+
+function rane(min, max, except) {
+    var a = Math.floor(Math.random() * (max - min + 1)) + min; 
+    while (a == except) {
+        a = Math.floor(Math.random() * (max - min + 1)) + min; 
+    }
+    return a;
+}
+
+function ran(min, max)  
+{  
+    return Math.floor(Math.random() * (max - min + 1)) + min;  
+} 
+
+function moveIt()
+{
+    var thisWidth = document.getElementById("canvas").getBoundingClientRect().width;
+    var thisHeight = document.getElementById("canvas").getBoundingClientRect().height;
+    //c.attr("path", "M" + ran(10, 100).toString() + " " + ran(10, 100).toString() + "L" + ran(100, 200).toString() + " " + ran(100, 200).toString());
+    for(i = 0; i < circs.length; ++i)
+    {
+        // Reset when time is at zero
+        if (! circs[i].time) 
+        {
+            circs[i].time  = ran(30, 100);
+            circs[i].deg   = ran(-179, 180);
+            circs[i].vel   = ran(1, 5);  
+            circs[i].curve = ran(0, 1);
+            circs[i].fade  = ran(0, 1);
+            circs[i].grow  = ran(-2, 2); 
+        }                
+        // Get position
+        nowX = circs[i].attr("cx");
+        nowY = circs[i].attr("cy");   
+        // Calc movement
+        dx = circs[i].vel * Math.cos(circs[i].deg * Math.PI/180);
+        dy = circs[i].vel * Math.sin(circs[i].deg * Math.PI/180);
+        // Calc new position
+        nowX += dx;
+        nowY += dy;
+        // Calc wrap around
+        if (nowX < 0) nowX = thisWidth - 10 + nowX;
+        else          nowX = nowX % (thisWidth - 10);            
+        if (nowY < 0) nowY = thisHeight - 10 + nowY;
+        else          nowY = nowY % (thisHeight - 10);
+
+        // Render moved particle
+        circs[i].attr({cx: nowX, cy: nowY});
+
+        // Calc growth
+        rad = circs[i].attr("r");
+        if (circs[i].grow > 0) circs[i].attr("r", 20);
+        else                   circs[i].attr("r", 20);
+
+        // Calc curve
+        if (circs[i].curve > 0) circs[i].deg = circs[i].deg + 2;
+        else                    circs[i].deg = circs[i].deg - 2;
+
+        // Calc opacity
+        opa = circs[i].attr("fill-opacity");
+        if (circs[i].fade > 0) {
+            circs[i].attr("fill-opacity", 1);
+            circs[i].attr("stroke-opacity", 1); }
+        else {
+            circs[i].attr("fill-opacity", 1);
+            circs[i].attr("stroke-opacity", 1); }
+
+        // Progress timer for particle
+        circs[i].time = circs[i].time - 1;
+
+        // Calc damping
+        if (circs[i].vel < 1) circs[i].time = 0;
+        else circs[i].vel = circs[i].vel - .05;              
+
+    }
+    for(j = 0; j < lineIds.length;j++) {
+        var thisLineIds = lineIds[j];
+        var thisLine =  lines[j];
+        var circ1 = circs[circIds.indexOf(thisLineIds[0])]
+        var circ2 = circs[circIds.indexOf(thisLineIds[1])]
+        thisLine.attr("path", "M" + circ1.attr("cx").toString() + " " + circ1.attr("cy").toString() + "L" + circ2.attr("cx").toString() + " " + circ2.attr("cy").toString());
+    }
+    timer = setTimeout(moveIt, 60);
+}
+
+function startup () {
+    if(typeof Raphael !== "undefined") {
+        paper = Raphael("canvas", "100%", "100%");
+        circs = paper.set();
+        lines = paper.set();
+        for (i = 0; i < 30; ++i) {
+            opa = ran(3,10)/10;
+            circs.push(paper.circle(ran(1000,2000), ran(1000,2000), 20).attr({"fill-opacity": 1, "stroke-opacity": 1}));
+            circIds.push(randomId());
+        }
+        circs.attr({fill: "#00DDAA", stroke: "#00DDAA"});
+        for (j = 0; j < 30; j++) {
+            var circ1 = circs[j];
+            var k = rane(0,29, j);
+            var circ2 = circs[k]
+            lines.push(paper.path("M" + circ1.attr("cx").toString() + " " + circ1.attr("cy").toString() + "L" + circ2.attr("cx").toString() + " " + circ2.attr("cy").toString()).attr({stroke: "#00DDAA"}));
+            lineIds.push([circIds[j], circIds[k]]);
+            console.log(j);
+        }
+        moveIt();
+    }
+    else {
+        setTimeout(function(){
+            startup();
+        }, 250)
+    }
 }
 
 Router.route("/flowchart");
@@ -10,21 +128,18 @@ Router.route("/", function() {
 
 
 if (Meteor.isClient) {
-    
   var startLocation = [-1, -1];
   Meteor.startup(function () {
       jsPlumb.ready(function() {
           jsPlumb.setContainer($("#flowContainer"));
       });
       $("#defaultradio").attr('checked', true);
-  })
+  });
     
-  Template.home.startup(function () {
-      jsPlumb.ready(function() {
-          jsPlumb.setContainer($("#bg"));
-      });
-      
-  })
+  Template.home.onRendered(function() {
+      startup();
+  });
+  
   
   Template.notetaker.events({
       'submit .new-bullet' : function (e) {
